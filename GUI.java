3da2c740bef472e2a0e3648cc950d3f64d71b326
nilswenning <nilswenning@web.de>
2018-01-12 09:49:54 +0100

    Add files via upload
    First release

---
+++ GUI.java
@@ -0,0 +1,46 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+/**
+  *
+  * Beschreibung
+  *
+  * @version 1.0 vom 12.01.2018
+  * @author 
+  */
+
+public class GUI extends JFrame {
+  // Anfang Attribute
+  // Ende Attribute
+  
+  public GUI(String title) { 
+    // Frame-Initialisierung
+    super(title);
+    setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    int frameWidth = 300; 
+    int frameHeight = 300;
+    setSize(frameWidth, frameHeight);
+    Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+    int x = (d.width - getSize().width) / 2;
+    int y = (d.height - getSize().height) / 2;
+    setLocation(x, y);
+    setResizable(false);
+    Container cp = getContentPane();
+    cp.setLayout(null);
+    // Anfang Komponenten
+    
+    // Ende Komponenten
+    
+    setVisible(true);
+  } // end of public GUI
+  
+  // Anfang Methoden
+  // Ende Methoden
+  
+  public static void main(String[] args) {
+    new GUI("GUI");
+  } // end of main
+  
+} // end of class GUI
