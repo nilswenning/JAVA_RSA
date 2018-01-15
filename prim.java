55c22c3c28608e43c86c772d5874b477aef1235e
nilswenning <nils@nilswe.de>
2018-01-15 19:52:08 +0100

    prim klasse erstellen

---
+++ prim.java
@@ -0,0 +1,23 @@
+/**
+  *
+  * Beschreibung
+  *
+  * @version 1.0 vom 15.01.2018
+  * @author 
+  */
+
+public class prim {
+  
+  // Anfang Attribute
+  // Ende Attribute
+  
+  public prim() {
+  }
+
+  // Anfang Methoden
+  public int prim() {
+    return 0;
+  }
+
+  // Ende Methoden
+} // end of prim
